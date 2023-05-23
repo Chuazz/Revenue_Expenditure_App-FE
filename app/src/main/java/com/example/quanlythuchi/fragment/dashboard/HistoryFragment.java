970f6932c3f751446bf677f2d0e4c38dba7a3e1f
Sơn Ngô <sonnv1912@gmail.com>
2023-05-23 09:44:01 +0700

    Fix HistoryFragment layout on load

--- app/src/main/java/com/example/quanlythuchi/fragment/dashboard/HistoryFragment.java
+++ app/src/main/java/com/example/quanlythuchi/fragment/dashboard/HistoryFragment.java
@@ -3,6 +3,7 @@ package com.example.quanlythuchi.fragment.dashboard;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.quanlythuchi.R;
@@ -23,20 +25,26 @@ import com.example.quanlythuchi.model.ChiPhi;
 import com.example.quanlythuchi.model.GiaoDich;
 import com.example.quanlythuchi.model.LichSu;
 import com.example.quanlythuchi.service.ChiPhiService;
+import com.example.quanlythuchi.service.LayoutService;
 import com.example.quanlythuchi.service.LichSuChiTieuService;
 import com.example.quanlythuchi.service.ThuNhapService;
+import com.example.quanlythuchi.service.shareService;
 import com.example.quanlythuchi.util.Commas;
 
 import org.bson.Document;
 import org.w3c.dom.Text;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class HistoryFragment extends Fragment {
     List<LichSu> lichSus = new ArrayList<>();
@@ -47,6 +55,9 @@ public class HistoryFragment extends Fragment {
     ThuNhapService thuNhapService;
     ChiPhiService chiPhiService;
     Document nguoiDung;
+    ProgressDialog progressDialog;
+    ImageView backBtn;
+    LayoutService layoutService;
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -73,21 +84,86 @@ public class HistoryFragment extends Fragment {
         thuNhapService = new ThuNhapService();
         chiPhiService = new ChiPhiService();
         nguoiDung = new Document("tenDangNhap", LoginActivity.nguoiDung.getTenDangNhap());
+        progressDialog = new ProgressDialog(getContext());
+        backBtn = view.findViewById(R.id.history_backBtn);
+        layoutService = new LayoutService(getParentFragmentManager());
 
+        progressDialog.setMessage("Đang lấy thông tin của bạn");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
 
         onCreate();
         setTotalPayReceive();
+        getHistory();
+        onClickBackBtn();
 
         return view;
+    }
+
+    void getHistory(){
+        LichSuChiTieuService lichSuChiTieuService = new LichSuChiTieuService();
+        Document document = new Document("tenDangNhap", LoginActivity.nguoiDung.getTenDangNhap());
+
+        CompletableFuture<Void> processedFuture = lichSuChiTieuService.getTransactionHistory(document).thenAccept(map -> {
+            TreeMap<Date, List<GiaoDich>> sortedMap = new TreeMap<>(Collections.reverseOrder());
+
+            sortedMap.putAll(map);
+
+            for (Map.Entry<Date, List<GiaoDich>> entry : sortedMap.entrySet()) {
+                Date key = entry.getKey();
+                List<GiaoDich> giaoDiches = entry.getValue();
+
+                LichSu lichSu = new LichSu(key, giaoDiches);
+
+                // Kiểm tra có phải ngày hôm nay không
+                if(key.compareTo(new Date()) == 0) {
+                    lichSu.setToday(true);
+                }
+
+                // Lấy thứ, ngày, tháng, năm
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(key);
+                lichSu.set_thu(shareService.getDayOfWeek(calendar.get(Calendar.DAY_OF_WEEK)));
+                lichSu.set_ngay(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)));
+                lichSu.set_thang(String.valueOf(calendar.get(Calendar.MONTH) + 1));
+                lichSu.set_nam(String.valueOf(calendar.get(Calendar.YEAR)));
+
+                // Tính tổng thu/chi trong 1 ngày
+                for (GiaoDich giaoDich : giaoDiches) {
+                    if(giaoDich.isThuNhap()) {
+                        lichSu.plusThu(giaoDich.getTien());
+                    }
+                    else {
+                        lichSu.plusChi(giaoDich.getTien());
+                    }
+                }
+
+                lichSus.add(lichSu);
+            }
+            listItem.post(() -> {
+                HistoryAdapter historyAdapter = new HistoryAdapter(lichSus);
+                listItem.setAdapter(historyAdapter);
+                progressDialog.cancel();
+            });
+        }).exceptionally(e -> {
+            Log.v("EXAMPLE", "Lỗi xảy ra trong quá trình lấy lịch sử chi tiêu!");
+            return null;
+        });
     }
 
     void onCreate(){
         Bundle bundle = getArguments();
 
-        if(bundle != null){
-            HistoryAdapter historyAdapter = new HistoryAdapter((List<LichSu>) bundle.getSerializable("lich_su"));
-            listItem.setAdapter(historyAdapter);
-        }
+//        if(bundle != null){
+//            HistoryAdapter historyAdapter = new HistoryAdapter((List<LichSu>) bundle.getSerializable("lich_su"));
+//            listItem.setAdapter(historyAdapter);
+//        }
+    }
+
+    void onClickBackBtn(){
+        backBtn.setOnClickListener(e -> {
+            layoutService.loadDashboard();
+        });
     }
 
     @SuppressLint("SetTextI18n")
